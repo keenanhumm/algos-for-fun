f25d3566e0caf2507dfcbd490b29700f45a5a48b
keenanhumm <keenan.humm@gmail.com>
2020-03-31 17:18:23 -0500

    binary_search.js

--- binary_search.js
+++ binary_search.js
@@ -1,20 +1,31 @@
-const findNum = (arr, target) => {
-  arr.sort();
+function searchNum(arr = [], target = null) {
+  const { length: n } = arr;
+  if (!target || !n) return -1;
+  let start = 0;
+  let end = n - 1;
 
-  let left = 0;
-  let right = arr.length-1;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] < target) 
-      left = mid + 1;
-    else if (arr[mid] > target) 
-      right = mid - 1;
-    else
-      return mid;
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    
+    if (arr[mid] === target) return mid;
+    else if (arr[mid] < target) start = mid;
+    else end = mid;
   }
 
   return -1;
 }
 
-console.log(findNum([2,3,5,67,2,1], 67));
+const testArr = [10, 33, 455, 521, 687, 689, 788, 798, 799];
+
+const tests = [
+  { target: 10, result: testArr.indexOf(10) },
+  { target: 521, result: testArr.indexOf(521) },
+  { target: 798, result: testArr.indexOf(798) }
+];
+
+tests.forEach((test, index) =>
+  console.log(
+    index,
+    searchNum(testArr, test.target) === test.result ? "pass" : "fail"
+  )
+);
