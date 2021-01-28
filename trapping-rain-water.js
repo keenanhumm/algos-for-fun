2aba3d96dc505f85fd065d82ba99aaf666f5fce1
Keenan Humm <keenan.humm@gmail.com>
2021-01-28 10:11:41 -0600

    includes trapping water

---
+++ trapping-rain-water.js
@@ -0,0 +1,37 @@
+// from: https://leetcode.com/problems/trapping-rain-water/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (heights) {
+  // max l and r barriers
+  let lMax = 0;
+  let rMax = 0;
+
+  // l and r pointers
+  let l = 0;
+  let r = heights.length - 1;
+
+  // trapped water so far
+  let trapped = 0;
+
+  // difference in height btwn max barrier and current pointer for both l and r
+  let lDiff = 0;
+  let rDiff = 0;
+
+  // until pointers meet
+  while (l < r) {
+    lMax = Math.max(lMax, heights[l]);
+    // a non-negative difference btwn max barrier and current pointer is equal to the increase in trapped water
+    trapped += lMax - heights[l];
+
+    rMax = Math.max(rMax, heights[r]);
+    trapped += rMax - heights[r];
+
+    // move the lesser pointer
+    heights[l] < heights[r] ? l++ : r--;
+  }
+
+  return trapped;
+};
